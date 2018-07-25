90a5c48b76f83cd79958963667d68f71140e0eee
Ricardo <ricardo.baldrez@gmail.com>
2018-07-25 01:46:59 -0300

    Inclusão do campo textArea e dos campos contador de caracteres e palavras no arquivo principal.html. E inclusão no main.js do evento de input no tetxArea para contar a quantidade de palavras e caracteres escritos no campo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
+/* Contando a quantidade de palavras na frase e atualizando no campo" */
 var frase = $(".frase").text();
-var numPalavras = frase.split(" ").lenght;
+var numPalavras = frase.split(" ").length;
 
 var tamanhoFrase = $("#tamanho-frase").text(numPalavras);
+
+/* Campo digitação (Text Área) */
+var campo = $("#campo-digitacao");
+campo.on("input", function() {
+
+    var conteudo = $(this).val();
+
+    /* Contagem das palavras da frase */
+    var qtdePalavras = conteudo.split(/\S+/).length -1;
+    $("#contador-palavras").text(qtdePalavras);
+
+    /* Contagem dos caracteres da frase */
+    var conteudoSemEspaco = conteudo.replace(/\s+/g,'');
+    var qtdeCaracteres = conteudoSemEspaco.length;
+    $("#contador-caracteres").text(qtdeCaracteres);
+
+});
